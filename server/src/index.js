f57fe55858e5b4c1f186427ce55c2cd321f4adc3
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-24 11:34:20 +0530

    Setup travis ci and heroku deployment

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = 3001;
-
+const port = process.env.PORT || 3001;
+const { log } = console;
 app.use(cors());
 
-app.get('/', (req, res) => res.json({ text: 'Hello World!' }));
+app.get('/api', (req, res) => res.json({ text: 'Hello World!' }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => log(`Example app listening on port ${port}!`));
