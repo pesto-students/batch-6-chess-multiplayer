ba5e3c9ab372f09dcf369ee5f9ffe240ccf6d1cf
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-27 02:12:33 +0530

    Setup router and bug fix in timerUtil.test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ChessGame from './components/chess-game/ChessGame';
+import Home from './components/home/Home';
 
-const App = () => (<ChessGame />);
+function App() {
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/chess-game" exact component={ChessGame} />
+    </Router>
+  );
+}
 
 export default App;
