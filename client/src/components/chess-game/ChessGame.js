bfa7fb3b6cc4d9158a4487ed68e86cb162c05dc5
Mayank <opthami@gmail.com>
2019-04-26 22:38:55 +0530

    create component structure for chess game

---
+++ client/src/components/chess-game/ChessGame.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ChessBoard from '../chess-board/ChessBoard';
+
+const ChessGame = () => (
+  <div>
+    ChessGame Component
+    <ChessBoard />
+  </div>
+);
+
+export default ChessGame;
