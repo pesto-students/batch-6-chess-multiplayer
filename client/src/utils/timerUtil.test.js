93ebbf26da9bd972e550b2107b4dc6b20e317076
Sagar Bakhtar <s.v.bakhtar@gmail.com>
2019-04-27 02:12:33 +0530

    Setup router and bug fix in timerUtil.test

--- client/src/utils/timerUtil.test.js
+++ client/src/utils/timerUtil.test.js
@@ -2,7 +2,7 @@ import { convertSecToMinSecStr } from './timeUtil';
 
 describe('convertSecToMinSecStr: Test timer convertion', () => {
   it('Should handle valid time', () => {
-    expect(convertSecToMinSecStr(140).toBe('02:20'));
+    expect(convertSecToMinSecStr(140)).toBe('02:20');
     expect(convertSecToMinSecStr(719)).toBe('11:59');
     expect(convertSecToMinSecStr(0)).toBe('00:00');
   });
