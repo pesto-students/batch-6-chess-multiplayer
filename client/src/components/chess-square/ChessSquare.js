bf2273f36bd31ac55214ab825e25005aec42444d
Mayank <opthami@gmail.com>
2019-04-27 01:58:50 +0530

    feat: chess squares 1. Add chess Square component 2. Add svgs for chess pieces 3. Add styling for chess square
    add tests for chess-pieces component
    
    convert ChessSquare to class component
    
    update render function

--- client/src/components/chess-square/ChessSquare.js
+++ client/src/components/chess-square/ChessSquare.js
@@ -1,5 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './ChessSquare.css';
+import chessPieces from './chess-pieces';
 
-const ChessSquare = () => (<div>ChessSquare Component</div>);
+class ChessSquare extends React.Component {
+  handleSqClick = () => {
+    const { handleSelect, gridPosition } = this.props;
+    handleSelect(gridPosition);
+  }
+
+  render() {
+    let piece = '';
+    const {
+      isDark, highlight, pieceType, pieceColor, gridPosition,
+    } = this.props;
+    const key = `${pieceType}${pieceColor}`;
+    if (Object.hasOwnProperty.call(chessPieces, key)) {
+      piece = chessPieces[key];
+    }
+    let squareClass = isDark ? 'dark-square' : 'light-square';
+    squareClass += highlight ? ' square-highlight' : '';
+
+    return (
+      <div
+        role="button"
+        tabIndex={0}
+        onKeyPress={this.handleSqClick}
+        onClick={this.handleSqClick}
+        grid-position={gridPosition}
+        className={squareClass}
+      >
+        {piece ? <img src={piece} alt="chess_piece" /> : null}
+      </div>
+    );
+  }
+}
+
+ChessSquare.propTypes = {
+  isDark: PropTypes.bool.isRequired,
+  gridPosition: PropTypes.string.isRequired,
+  pieceType: PropTypes.string,
+  pieceColor: PropTypes.string,
+  highlight: PropTypes.bool.isRequired,
+  handleSelect: PropTypes.func.isRequired,
+};
+
+ChessSquare.defaultProps = {
+  pieceType: '',
+  pieceColor: '',
+};
 
 export default ChessSquare;
