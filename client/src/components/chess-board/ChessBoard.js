51ce6bf41610bf10864f77459f6ff34dfeeb21de
Mayank <opthami@gmail.com>
2019-04-27 08:19:09 +0530

    add chessboard component
    refactor initail board state test
    
    refactor chess board test
    
    change moves function and refactor chessboard test
    
    use class variables and use arrow functions as class methods
    
    modify eslintrc

--- client/src/components/chess-board/ChessBoard.js
+++ client/src/components/chess-board/ChessBoard.js
@@ -1,11 +1,87 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './ChessBoard.css';
 import ChessSquare from '../chess-square/ChessSquare';
 
-const ChessBoard = () => (
-  <div>
-    ChessBoard Component
-    <ChessSquare />
-  </div>
-);
+const matchingStringInArray = array => str => array.some(x => x.includes(str));
+
+class ChessBoard extends React.Component {
+  state = {
+    possibleMoves: [],
+    from: '',
+    prevSelectValidTouch: false,
+  };
+
+  generateGrid = (playerColor, squares, board) => {
+    if (board.length === 0) {
+      return null;
+    }
+    const { possibleMoves } = this.state;
+    const isValidPosition = matchingStringInArray(possibleMoves);
+    return squares.map((position, index) => {
+      const row = parseInt(index / 8, 10);
+      const col = index % 8;
+      const isDark = row % 2 === 0 ? col % 2 === 1 : col % 2 === 0;
+      const { type = null, color = null } = board[row][col] || {};
+      const highlight = isValidPosition(position);
+
+      return (
+        <ChessSquare
+          key={position}
+          handleSelect={this.handleSquareClick}
+          gridPosition={position}
+          isDark={isDark}
+          pieceType={type}
+          pieceColor={color}
+          highlight={highlight}
+        />
+      );
+    });
+  }
+
+  handleSquareClick = (gridPosition) => {
+    const { possibleMoves, prevSelectValidTouch, from } = this.state;
+    const { movePiece } = this.props;
+    const isValidMove = matchingStringInArray(possibleMoves)(gridPosition);
+
+    if (prevSelectValidTouch && isValidMove) {
+      movePiece(from, gridPosition);
+      this.setState({ possibleMoves: [], from: '', prevSelectValidTouch: false });
+    } else {
+      this.updatePossibleMoves(gridPosition);
+    }
+  }
+
+  updatePossibleMoves = (gridPosition) => {
+    const { calcPossibleMoves } = this.props;
+    const possibleMoves = calcPossibleMoves({ square: gridPosition });
+    if (possibleMoves.length > 0) {
+      this.setState({ possibleMoves, from: gridPosition, prevSelectValidTouch: true });
+    }
+  }
+
+  render() {
+    const { playerColor, squares, board } = this.props;
+    return (
+      <div className="chess-board">
+        {this.generateGrid(playerColor, squares, board)}
+      </div>
+    );
+  }
+}
+
+ChessBoard.propTypes = {
+  calcPossibleMoves: PropTypes.func.isRequired,
+  movePiece: PropTypes.func.isRequired,
+  playerColor: PropTypes.string,
+  squares: PropTypes.array,
+  board: PropTypes.array,
+};
+
+ChessBoard.defaultProps = {
+  playerColor: 'white',
+  squares: [],
+  board: [],
+};
 
 export default ChessBoard;
---
+++ client/src/components/chess-board/ChessBoard.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Chess } from 'chess.js';
+import ChessBoard from './ChessBoard';
+
+const chess = new Chess();
+const squares = chess.SQUARES;
+const board = chess.board();
+
+describe('<ChessBoard />', () => {
+  describe('generateGrid()', () => {
+    const wrapper = shallow(<ChessBoard />);
+    const instance = wrapper.instance();
+    test('generateGrid() should exist', () => {
+      expect(Object.hasOwnProperty.call(instance, 'generateGrid')).toBe(true);
+    });
+
+    test('should return null if board is empty array', () => {
+      const grid = instance.generateGrid('white', squares, []);
+      expect(grid).toBeNull();
+    });
+    const grid = instance.generateGrid('white', squares, board);
+
+    test('should return grid containing 64 chess squares', () => {
+      expect(grid.length).toBe(64);
+      expect(grid[0].type.name).toBe('ChessSquare');
+    });
+  });
+
+  describe('handleSquareClick()', () => {
+    const { moves } = chess;
+    const movePiece = (from, to) => {
+      chess.move(to);
+    };
+    const wrapper = shallow(
+      <ChessBoard
+        calcPossibleMoves={moves}
+        movePiece={movePiece}
+        squares={squares}
+        board={board}
+      />,
+    );
+    const { handleSquareClick } = wrapper.instance();
+    const testBoardState = (nextGridPosition, from, prevSelectValidTouch) => {
+      handleSquareClick(nextGridPosition);
+      expect(wrapper.state('from')).toBe(from);
+      expect(wrapper.state('prevSelectValidTouch')).toBe(prevSelectValidTouch);
+    };
+
+    test('should set the state property \'from\' to the provided gridPosition object when square with piece is clicked', () => {
+      const gridPosition = 'e2';
+      testBoardState(gridPosition, 'e2', true);
+    });
+
+    test('should reset state when piece is moved', () => {
+      expect(wrapper.state('from')).toBe('e2');
+      testBoardState('e3', '', false);
+    });
+
+    test('should not change the state when square with no valid moves is clicked', () => {
+      expect(wrapper.state('from')).toBe('');
+      testBoardState('e7', 'e7', true);
+      testBoardState('e3', 'e7', true);
+    });
+  });
+});
