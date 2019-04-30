0ca42fb5720d7e33ceb126f1fd52ca3804150e15
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-05-01 02:58:23 +0530

    add login component.
    fix:add background to login component only

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ChessGame from './components/chess-game/ChessGame';
 import Home from './components/home/Home';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <Router>
+      <Route path="/login" exact component={Login} />
       <Route path="/" exact component={Home} />
       <Route path="/chess-game" exact component={ChessGame} />
     </Router>
---
+++ client/src/components/login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './login.css';
+
+const Login = () => (
+  <div id="content">
+    <p>Multiplayer Chess</p>
+  </div>
+);
+
+export default Login;
