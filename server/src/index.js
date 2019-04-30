ea6769852fb9f4f56cc55680b8fa5d8788ebf011
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-05-01 02:30:54 +0530

    Setup database connection
    Added dotenv dependency
    Added mongoose dependency
    created db connection configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import 'dotenv/config';
 import cors from 'cors';
+import db from './config/database';
+import Config from './config/config';
+
+db.connectDb();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = Config.server.SERVER_PORT;
 const { log } = console;
 app.use(cors());
 
 app.get('/api', (req, res) => res.json({ text: 'Hello World!' }));
 
-app.listen(port, () => log(`Example app listening on port ${port}!`));
+app
+  .listen(port, () => log(`Example app listening on port ${port}!`))
+  .on('close', () => db.disconnectDb());
