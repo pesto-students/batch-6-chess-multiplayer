9b8a2628fb633f2675a007936b8ccd144fb46b32
Chirag Shinde <shinde.chirag2204@gmail.com>
2019-05-01 05:41:02 +0530

    add login with google component

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './login.css';
+import GoogleButton from './google-button/GoogleButton';
 
 const Login = () => (
   <div id="content">
     <p>Multiplayer Chess</p>
+    <GoogleButton />
   </div>
 );
 
