fac5abab5ec0be2c5c88a9d031e927646cfad878
Mayank <opthami@gmail.com>
2019-05-03 01:03:44 +0530

    fix timer issue

--- client/src/config/chess-game.js
+++ client/src/config/chess-game.js
@@ -1,3 +1,3 @@
-export const WHITE_PLAYER = 'white_player';
-export const BLACK_PLAYER = 'black_player';
+export const WHITE_PLAYER = 'w';
+export const BLACK_PLAYER = 'b';
 export const DEFAULT_GAME_TIME_SECS = 60;
